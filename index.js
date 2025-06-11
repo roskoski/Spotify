6e6b14c7457a534cd9c739cb6772408cf590d402
Eduardo Roskoski <eduardo.roskoski.oliveira@escola.pr.gov.br>
2025-06-11 11:25:27 -0300

    index

--- index.js
+++
@@ -1,6 +0,0 @@
-function login() {
-  // This is a placeholder.  You'll need server-side code to handle actual login
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-  alert(`Logging in with username: ${username}`); // Replace with actual login logic
-}
